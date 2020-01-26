dccd61f8943250732b179225adc09e0fcf7ab28b
smilu97 <smilup2244@gmail.com>
2020-01-26 23:17:19 +0900

    CONSTANTIFY a message 'The server is dead'

--- minecraft.py
+++ minecraft.py
@@ -4,6 +4,8 @@ from mcstatus import MinecraftServer
 
 server = MinecraftServer.lookup('15.165.167.41:25565')
 
+MSG_SERVER_IS_DEAD = 'The server is dead'
+
 def check_server_dead():
     try:
         latency = server.ping()
@@ -21,19 +23,19 @@ def get_minecraft_query():
     return server.query()
 
 def get_minecraft_status_message():
-    if check_server_dead(): return 'Server is dead'
+    if check_server_dead(): return MSG_SERVER_IS_DEAD
     status = get_minecraft_status()
     message = "The server has {0} players and replied in {1} ms".format(status.players.online, status.latency)
     return message
 
 def get_minecraft_ping_message():
-    if check_server_dead(): return 'Server is dead'
+    if check_server_dead(): return MSG_SERVER_IS_DEAD
     ping = get_minecraft_ping()
     message = "The server replied in {0} ms".format(ping)    
     return message
 
 def get_minecraft_query_message():
-    if check_server_dead(): return 'Server is dead'
+    if check_server_dead(): return MSG_SERVER_IS_DEAD
     query = get_minecraft_query()
     message = "The server has the following players online: {0}".format(", ".join(query.players.names))
 
